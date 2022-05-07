81114fb96ceee489044fa060108f6976d0f101f3
sariya23 <dayzwaitik123@yandex.ru>
2022-05-08 01:18:16 +0300

    Файлы создаются отонсительно имени, каждому профилю свои данные, реализована функция write_data

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ class Profile:
     def __init__(self, name):  # конструктор
         self.name = name
         self.data = []
+        self.data_string = ''
         self.file_name = str(self.name)
         self.f = open(self.file_name + '.txt', 'w+')
-        self.f.writelines(self.name)
-        self.f.close()
+        self.f.writelines(self.name + '\n')
 
     def write_data(self, arr):  # записать данные
         self.data.extend(arr)
-
+        self.data_string = ''
+        
+        for i in self.data:
+            self.data_string += str(i)
+        self.f.writelines(self.data_string)
+        
     def delete_data(self, deleted_item):  # удалить данные
         del self.data[self.data.index(deleted_item)]
 
@@ -18,8 +23,10 @@ class Profile:
 
 
 a = Profile('Nikita')
-b = Profile('MAx')
+a.write_data([1, 2, 3])
 
+b = Profile('Max')
+b.write_data([4, 5, 6])
 
 
 
